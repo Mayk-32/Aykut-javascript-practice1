ce8e2f27791ac07e4df5fa4fb4d10250cad0549b
Aykut <m.aykutarslan1@gmail.com>
2026-02-07 00:52:36 +0000

    javascript-pracctice-1

--- src/app.js
+++ src/app.js
@@ -7,5 +7,52 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Aykut from the console!");
+
+  let age = 24;
+  console.log(age);
+
+  age = 37;
+
+
+  const newAge = 32
+
+  console.log(newAge);
+
+
+  console.log(typeof(age));
+
+  let myName = "Aykut";
+  console.log(myName);
+  console.log(typeof(myName));
+
+
+  let iDontHaveAValue;
+  console.log(iDontHaveAValue);
+  console.log(typeof(iDontHaveAValue));
+
+
+  let iAmNull = null;
+  console.log(iAmNull);
+  console.log(typeof(iAmNull));
+
+  let my Array = [1, 2, 3];
+  console.log(myArray);
+  console.log(typeof(myArray));
+
+  let car = {
+
+    make: "Ford",
+    model: "Mustang",
+    year: "1979"
+
+  }
+
+  console.log(car);
+  console.log(typeof(car));
+
+
+
+
+
 };
